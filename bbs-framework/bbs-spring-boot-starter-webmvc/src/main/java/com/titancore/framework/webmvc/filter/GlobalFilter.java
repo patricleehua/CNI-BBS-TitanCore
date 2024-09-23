c1b0ea05d776fb69aaec1f234ab89e47b6cb356d
LBH <cyhlbh2010@gmail.com>
2024-09-23 10:23:08 +0800

    fix: 修复白名单无法匹配通配符问题

--- bbs-framework/bbs-spring-boot-starter-webmvc/src/main/java/com/titancore/framework/webmvc/filter/GlobalFilter.java
+++ bbs-framework/bbs-spring-boot-starter-webmvc/src/main/java/com/titancore/framework/webmvc/filter/GlobalFilter.java
@@ -1,118 +1,132 @@
-//package com.titancore.framework.webmvc.filter;
-//
-//
-//import com.titancore.framework.common.properties.AESKey;
-//import com.titancore.framework.common.properties.AesUtils;
-//import com.titancore.framework.common.util.Base64UrlUtils;
-//import com.titancore.framework.webmvc.interceptor.ModifiedHttpServletRequestWrapper;
-//import jakarta.servlet.*;
-//import jakarta.servlet.annotation.WebFilter;
-//import jakarta.servlet.http.HttpServletRequest;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.nio.charset.StandardCharsets;
-//import java.util.Base64;
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//@Slf4j
-//@WebFilter(filterName = "GlobalFilter", urlPatterns = {"/**"})
-//@Component
-//public class GlobalFilter implements Filter {
-//
-//    private static final Set<String> WHITELIST_PATHS = new HashSet<>();
-//
-//    static {
-//        WHITELIST_PATHS.add("/system/verif/gen/random");
-//        WHITELIST_PATHS.add("/system/verif/check3");
-//        WHITELIST_PATHS.add("/common/upload");
-//    }
-//
-//    @Autowired
-//    private AESKey aesKey;
-//
-//    @Override
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-//            throws IOException, ServletException {
-//        HttpServletRequest httpRequest = (HttpServletRequest) request;
-//
-//        // 上传文件不做解密处理
-//        if ("multipart/form-data".equals(httpRequest.getHeader("Content-Type"))) {
-//            chain.doFilter(request, response);
-//            return;
-//        }
-//
-//        // 检查请求路径是否在白名单中
-//        String requestURI = httpRequest.getRequestURI();
-//        if (WHITELIST_PATHS.contains(requestURI)) {
-//            chain.doFilter(request, response);
-//            return;
-//        }
-//
-//        // 只拦截POST请求
-//        if (!"POST".equalsIgnoreCase(httpRequest.getMethod())) {
-//            chain.doFilter(request, response);
-//            return; // 放行非POST请求，不再执行后续逻辑
-//        }
-//
-//        // 读取请求体
-//        String requestBody = readRequestBody(httpRequest);
-//
-//        // 处理双引号
-//        requestBody = requestBody.replace("\"", ""); // 可选择性移除双引号，如果需要
-//
-//        // 获取IV
-//        String IV = httpRequest.getHeader("iv");
-//        if (IV == null || IV.isEmpty()) {
-//            return; //非IV加密
-////            throw new IllegalRequestException("不合法的请求：缺少IV");
-//        }
-//        String decodeBase64Body = Base64UrlUtils.base64UrlToBase64(requestBody);
-//        String decodeBase64BodyIV = Base64UrlUtils.base64UrlToBase64(IV);
-//        // 解密请求体
-//        byte[] decrypt = decrypt(decodeBase64Body
-//                ,decodeBase64BodyIV );
-//
-//        String decryptedString = new String(decrypt, StandardCharsets.UTF_8);
-//
-//        // 使用修改后的请求体创建新的请求对象
-//        ModifiedHttpServletRequestWrapper modifiedRequest = new ModifiedHttpServletRequestWrapper(httpRequest, decryptedString);
-//
-//        // 继续处理请求
-//        chain.doFilter(modifiedRequest, response);
-//    }
-//
-//    private byte[] decrypt(String encryptedText, String IV) throws IOException {
-//        try {
-//            byte[] key = Base64.getDecoder().decode(aesKey.getSecretKey());
-//            byte[] iv = Base64.getDecoder().decode(IV);
-//            byte[] encryptedData = Base64.getDecoder().decode(encryptedText);
-//            return AesUtils.decrypt(key, iv, encryptedData);
-//        } catch (Exception e) {
-//            throw new IOException("解密失败", e);
-//        }
-//    }
-//
-//    private String readRequestBody(HttpServletRequest request) throws IOException {
-//        // 读取请求体的内容
-//        StringBuilder stringBuilder = new StringBuilder();
-//        try (BufferedReader reader = request.getReader()) {
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                stringBuilder.append(line);
-//            }
-//        }
-//        return stringBuilder.toString();
-//    }
-//
+package com.titancore.framework.webmvc.filter;
+
+
+import com.titancore.framework.common.properties.AESKey;
+import com.titancore.framework.common.properties.AesUtils;
+import com.titancore.framework.common.util.Base64UrlUtils;
+import com.titancore.framework.webmvc.interceptor.ModifiedHttpServletRequestWrapper;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashSet;
+import java.util.Set;
+
+@Slf4j
+@WebFilter(filterName = "GlobalFilter", urlPatterns = {"/**"})
+@Component
+public class GlobalFilter implements Filter {
+
+    private static final Set<String> WHITELIST_PATHS = new HashSet<>();
+
+    static {
+        WHITELIST_PATHS.add("/system/verif/gen/random");
+        WHITELIST_PATHS.add("/system/verif/check3");
+        WHITELIST_PATHS.add("/common/upload");
+        WHITELIST_PATHS.add("/favicon.ico");
+        WHITELIST_PATHS.add("/swagger-resources/**");
+        WHITELIST_PATHS.add("/webjars/**");
+        WHITELIST_PATHS.add("/v3/**");
+        WHITELIST_PATHS.add("/doc.html");
+    }
+
+    @Autowired
+    private AESKey aesKey;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        // 上传文件不做解密处理
+        if ("multipart/form-data".equals(httpRequest.getHeader("Content-Type"))) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // 检查请求路径是否在白名单中
+        String requestURI = httpRequest.getRequestURI();
+        for (String path : WHITELIST_PATHS) {
+            if(path.endsWith("/**")){
+                String prePath = path.substring(0, path.length() - 3);
+                if(requestURI.startsWith(prePath)){
+                    chain.doFilter(request, response);
+                    return;
+                }
+            } else if (requestURI.equals(path)) {
+                chain.doFilter(request, response);
+                return;
+            }
+        }
+
+
+        // 只拦截POST请求
+        if (!"POST".equalsIgnoreCase(httpRequest.getMethod())) {
+            chain.doFilter(request, response);
+            return; // 放行非POST请求，不再执行后续逻辑
+        }
+
+        // 读取请求体
+        String requestBody = readRequestBody(httpRequest);
+
+        // 处理双引号
+        requestBody = requestBody.replace("\"", ""); // 可选择性移除双引号，如果需要
+
+        // 获取IV
+        String IV = httpRequest.getHeader("iv");
+        if (IV == null || IV.isEmpty()) {
+            return; //非IV加密
+//            throw new IllegalRequestException("不合法的请求：缺少IV");
+        }
+        String decodeBase64Body = Base64UrlUtils.base64UrlToBase64(requestBody);
+        String decodeBase64BodyIV = Base64UrlUtils.base64UrlToBase64(IV);
+        // 解密请求体
+        byte[] decrypt = decrypt(decodeBase64Body
+                ,decodeBase64BodyIV );
+
+        String decryptedString = new String(decrypt, StandardCharsets.UTF_8);
+
+        // 使用修改后的请求体创建新的请求对象
+        ModifiedHttpServletRequestWrapper modifiedRequest = new ModifiedHttpServletRequestWrapper(httpRequest, decryptedString);
+
+        // 继续处理请求
+        chain.doFilter(modifiedRequest, response);
+    }
+
+    private byte[] decrypt(String encryptedText, String IV) throws IOException {
+        try {
+            byte[] key = Base64.getDecoder().decode(aesKey.getSecretKey());
+            byte[] iv = Base64.getDecoder().decode(IV);
+            byte[] encryptedData = Base64.getDecoder().decode(encryptedText);
+            return AesUtils.decrypt(key, iv, encryptedData);
+        } catch (Exception e) {
+            throw new IOException("解密失败", e);
+        }
+    }
+
+    private String readRequestBody(HttpServletRequest request) throws IOException {
+        // 读取请求体的内容
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = request.getReader()) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
 //    public static void main(String[] args) {
 //        try {
 //            // Base64 解码 IV
-//            String ivBase64 = "eCV9XNIF7JcdpwBpoejhIw";
+//            String ivBase64 = "SfM1WDtbLKneSEvOSiGpkg==";
 //            byte[] iv = Base64.getDecoder().decode(Base64UrlUtils.base64UrlToBase64(ivBase64));
 //
 //            // Base64 解码密钥
@@ -120,7 +134,7 @@
 //            byte[] key = Base64.getDecoder().decode(secretKey);
 //
 //            // Base64 解码加密数据
-//            String encryptedBase64 = "KVfRQ8Dlx_053j7SFxlX66ieDIH7H71pdM4a0f6IFsI6nalCkoKOnZucFNxs6zC2nVBstVrIyR7BtcVHM3W4I2Csewf9aPV4BZFXlxXlF1vthZLAkQ";
+//            String encryptedBase64 = "8MuzAHhtkSZ0Y3q6B8BX6bLHguTVmBLamHpNe5/LVD/Q0xmoeFmnh8jClddGw3ncen9xK7ZnlmZOtPmTiMpo6vFkdFSFo4CV0JwJIJzLtNmdPKowz0adnfo/ryNrE4P5C2iqcJvrbFHYp4VErm9Dyas/1tHveyyZNmTjiypn/LH7yKyGfCyLiuKlNw8xLYk9bMtWknzkJWPI+nB1XdoWe8TuNAn6mFNtz0xMm0VRe3gng5N5RUIC/Xw8z8HY44M5VESnIgLM6WjeNoIb7VWT83jFdpT25+fLzYOhew==";
 //            byte[] encryptedData = Base64.getDecoder().decode(Base64UrlUtils.base64UrlToBase64(encryptedBase64));
 //
 //            // 解密
@@ -132,5 +146,5 @@
 //            e.printStackTrace();
 //        }
 //    }
-//}
-//
+}
+
--- bbs-framework/bbs-spring-boot-starter-webmvc/src/main/java/com/titancore/framework/webmvc/interceptor/ResponseEncryptionAdvice.java
+++ bbs-framework/bbs-spring-boot-starter-webmvc/src/main/java/com/titancore/framework/webmvc/interceptor/ResponseEncryptionAdvice.java
@@ -1,87 +1,99 @@
-//package com.titancore.framework.webmvc.interceptor;
-//
-//
-//import com.alibaba.fastjson.JSON;
-//import com.titancore.framework.common.properties.AESKey;
-//import com.titancore.framework.common.properties.AesUtils;
-//import com.titancore.framework.common.util.Base64UrlUtils;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.core.MethodParameter;
-//import org.springframework.http.MediaType;
-//import org.springframework.http.converter.HttpMessageConverter;
-//import org.springframework.http.server.ServerHttpRequest;
-//import org.springframework.http.server.ServerHttpResponse;
-//import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
-//
-//import java.nio.charset.StandardCharsets;
-//import java.security.SecureRandom;
-//import java.util.Base64;
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//@ControllerAdvice
-//public class ResponseEncryptionAdvice implements ResponseBodyAdvice<Object> {
-//
-//    @Autowired
-//    private AESKey aesKey;
-//
-//    // 白名单路径集合
-//    private static final Set<String> WHITELIST_PATHS = new HashSet<>();
-//
-//    static {
-//        // 在这里添加需要放行的路径
-//        WHITELIST_PATHS.add("/system/verif/gen/random");
-//        WHITELIST_PATHS.add("/system/verif/check3");
-//        WHITELIST_PATHS.add("/common/upload");
-//    }
-//
-//    @Override
-//    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
-//        // 决定是否需要加密响应体
-//        return true;
-//    }
-//
-//    @Override
-//    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
-//                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
-//                                  ServerHttpRequest request, ServerHttpResponse response) {
-//        String requestURI = request.getURI().getPath();
-//
-//        // 判断是否需要跳过加密
-//        if (WHITELIST_PATHS.contains(requestURI)) {
-//            return body; // 跳过加密
+package com.titancore.framework.webmvc.interceptor;
+
+
+import com.alibaba.fastjson.JSON;
+import com.titancore.framework.common.properties.AESKey;
+import com.titancore.framework.common.properties.AesUtils;
+import com.titancore.framework.common.util.Base64UrlUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.HashSet;
+import java.util.Set;
+
+@ControllerAdvice
+public class ResponseEncryptionAdvice implements ResponseBodyAdvice<Object> {
+
+    @Autowired
+    private AESKey aesKey;
+
+    // 白名单路径集合
+    private static final Set<String> WHITELIST_PATHS = new HashSet<>();
+
+    static {
+        // 在这里添加需要放行的路径
+        WHITELIST_PATHS.add("/system/verif/gen/random");
+        WHITELIST_PATHS.add("/system/verif/check3");
+        WHITELIST_PATHS.add("/common/upload");
+        WHITELIST_PATHS.add("/favicon.ico");
+        WHITELIST_PATHS.add("/swagger-resources/**");
+        WHITELIST_PATHS.add("/webjars/**");
+        WHITELIST_PATHS.add("/v3/**");
+        WHITELIST_PATHS.add("/doc.html");
+    }
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        // 决定是否需要加密响应体
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        String requestURI = request.getURI().getPath();
+
+        // 判断是否需要跳过加密
+        for (String path : WHITELIST_PATHS) {
+            if(path.endsWith("/**")){
+                String prePath = path.substring(0, path.length() - 3);
+                if(requestURI.startsWith(prePath)){
+                    return body;
+                }
+            } else if (requestURI.equals(path)) {
+                return body;
+            }
+        }
+        //是否为POST,不是POST不加密
+//        if (!"POST".equalsIgnoreCase(String.valueOf(request.getMethod()))) {
+//            return body; // 放行非POST请求，不再执行后续逻辑
 //        }
-//        //是否为POST,不是POST不加密
-////        if (!"POST".equalsIgnoreCase(String.valueOf(request.getMethod()))) {
-////            return body; // 放行非POST请求，不再执行后续逻辑
-////        }
-//        // 生成新的 IV
-//        byte[] iv = new byte[16];
-//        new SecureRandom().nextBytes(iv);
-//
-//        // 设置 IV 到响应头
-//        response.getHeaders().add("iv",
-//                Base64UrlUtils
-//                        .base64ToBase64Url(
-//                                Base64.getEncoder().encodeToString(iv)));
-//
-//        // 加密响应体
-//        if (body != null) {
-//            try {
-//                String responseBody = JSON.toJSONString(body);
-//                byte[] encryptedData = AesUtils.encrypt(Base64.getDecoder().decode(aesKey.getSecretKey()), iv, responseBody.getBytes());
-//                    return Base64UrlUtils
-//                        .base64ToBase64Url(
-//                                Base64.getEncoder().encodeToString(encryptedData));
-//            } catch (Exception e) {
-//                throw new RuntimeException("Encryption failed", e);
-//            }
-//        }
-//
-//        return body;
-//    }
-//
+        // 生成新的 IV
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+
+        // 设置 IV 到响应头
+        response.getHeaders().add("iv",
+                Base64UrlUtils
+                        .base64ToBase64Url(
+                                Base64.getEncoder().encodeToString(iv)));
+
+        // 加密响应体
+        if (body != null) {
+            try {
+                String responseBody = JSON.toJSONString(body);
+                byte[] encryptedData = AesUtils.encrypt(Base64.getDecoder().decode(aesKey.getSecretKey()), iv, responseBody.getBytes());
+                    return Base64UrlUtils
+                        .base64ToBase64Url(
+                                Base64.getEncoder().encodeToString(encryptedData));
+            } catch (Exception e) {
+                throw new RuntimeException("Encryption failed", e);
+            }
+        }
+
+        return body;
+    }
+
 //    public static void main(String[] args) {
 //        try {
 //            // 生成新的 IV
@@ -93,10 +105,21 @@
 //            byte[] key = Base64.getDecoder().decode(secretKey);
 //
 //            // 要加密的内容
-//            String body = "{\n" +
-//                    "  \"username\": \"spring\",\n" +
-//                    "  \"password\": \"123456\"\n" +
-//                    "}";
+//            String body =
+//                    """
+//                            {
+//                              "username": "spring",
+//                              "password": "123456",
+//                              "rememberMe": "1",
+//                              "loginType": "password",
+//                              "phoneNumber": "13711022832",
+//                              "emailNumber": "1806411@qq.com",
+//                              "captchaType": "email",
+//                              "captchaCode": "203607"
+//                            }
+//
+//                            """;
+//
 //            byte[] data = body.getBytes(StandardCharsets.UTF_8);
 //
 //
@@ -111,5 +134,5 @@
 //            e.printStackTrace();
 //        }
 //    }
-//
-//}
+
+}
