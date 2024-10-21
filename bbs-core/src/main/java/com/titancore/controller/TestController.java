e3dfe0b4c22d6132ade6d1ccf24c28fdfb500946
patricLee <cyhlbh2010@gmail.com>
2024-10-21 15:10:15 +0800

    fix: remove test

--- bbs-core/src/main/java/com/titancore/controller/TestController.java
+++ bbs-core/src/main/java/com/titancore/controller/TestController.java
@@ -1,43 +1,19 @@
 package com.titancore.controller;
 
-import com.titancore.framework.cloud.manager.config.CloudServiceFactory;
-import com.titancore.framework.cloud.manager.domain.dto.FileDelDTO;
+import com.titancore.framework.common.response.Response;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Tag(name = "测试")
+@Slf4j
+@RequestMapping("/test")
 public class TestController {
-    @Resource
-    private CloudServiceFactory factory;
-
-//    @PostMapping("/test")
-//    public String test(@RequestParam("file") MultipartFile file, @RequestParam("userId") long userId) throws IOException {
-//        var cloudStorageService = factory.createService();
-//        return cloudStorageService.uploadFile(    file.getBytes(),userId,"text.txt");
-//    }
-
-//    @GetMapping("/test")
-//    public  ResponseEntity<byte[]> test() throws IOException {
-//        var cloudStorageService = factory.createService();
-//        FileDowloadDTO fileDowloadDTO = new FileDowloadDTO();
-//        fileDowloadDTO.setFileName("text.txt");
-//        fileDowloadDTO.setUserId("12");
-//        byte[] fileBytes  = cloudStorageService.exportOssFile(fileDowloadDTO);
-//        HttpHeaders headers = new HttpHeaders();
-//        return new ResponseEntity<>(fileBytes, headers, HttpStatus.OK);
-//    }
-
-        @GetMapping("/test")
-    public boolean test() throws IOException {
-        var cloudStorageService = factory.createService();
-        FileDelDTO fileDelDTO = new FileDelDTO();
-            fileDelDTO.setFileName("text.txt");
-            fileDelDTO.setUserId("12");
-            fileDelDTO.setPath("file/");
-        return cloudStorageService.deleteByPath(fileDelDTO);
+    @GetMapping("/hello")
+    public Response<?> test(){
+        return Response.success("hello,test!");
     }
 }
